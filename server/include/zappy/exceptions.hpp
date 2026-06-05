58d45feda20abd24c4424042973c403657bfa894
Bard-Gaming <christophe@dronne.fr>
2026-06-05 16:08:35 +0200

    feat(server): add custom exceptions

---
+++ server/include/zappy/exceptions.hpp
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2026
+** Project - Zappy
+** File description:
+** Utility header file
+** for custom exceptions
+*/
+
+#pragma once
+
+#include "../../src/exceptions/BaseException.hpp"
+#include "../../src/exceptions/SocketException.hpp"
