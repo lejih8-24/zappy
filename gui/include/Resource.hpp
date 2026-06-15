620496325dd6ea9035eb23b21379a053de3cce22
Adrien <adrien.raphanaud@epitech.eu>
2026-06-15 18:48:46 +0200

    feat(gui): resource enum class

---
+++ gui/include/Resource.hpp
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2026
+** Project - Zappy
+** File description:
+** GUI resources storage types
+*/
+
+#pragma once
+
+#include <array>
+#include <cstddef>
+
+namespace Gui {
+
+enum class Resource {
+    Food,
+    Linemate,
+    Deraumere,
+    Sibur,
+    Mendiane,
+    Phiras,
+    Thystame,
+    Resource_Count, //? KEEP IT ALWAYS IN LAST -> useful to get the enum count
+};
+
+}
