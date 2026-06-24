21809c5178d9889d1e7801dc5f5c33a9058f809a
Bard-Gaming <christophe@dronne.fr>
2026-06-24 04:49:59 +0200

    feat(server): update header files

--- server/include/zappy.hpp
+++ server/include/zappy.hpp
@@ -8,6 +8,7 @@
 
 #pragma once
 
+#include "zappy/client.hpp"
 #include "zappy/exceptions.hpp"
 #include "zappy/game.hpp"
 #include "zappy/utils.hpp"
---
+++ server/include/zappy/client.hpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2026
+** Project - Zappy
+** File description:
+** Utility header file for
+** zappy's client implementation
+*/
+
+#pragma once
+
+#include "../../src/client/Client/Client.hpp"
+#include "../../src/client/GuiState/GuiState.hpp"
+#include "../../src/client/HandshakeState/HandshakeState.hpp"
+#include "../../src/client/PlayerRunState/PlayerRunState.hpp"
+#include "../../src/client/IState.hpp"
