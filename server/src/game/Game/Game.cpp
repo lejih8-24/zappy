f19209f7550660250fefa1c3e1375416966fb210
Bard-Gaming <christophe@dronne.fr>
2026-06-25 16:42:56 +0200

    feat(server): continue work on Game class

--- server/src/game/Game/Game.cpp
+++ server/src/game/Game/Game.cpp
@@ -7,9 +7,39 @@
 */
 
 #include "Game.hpp"
+#include <zappy/game.hpp>
+#include <random>
+
+
+std::mt19937 Zappy::Game::Game::s_RNG(std::random_device{}());
 
 
 Zappy::Game::Game::Game() noexcept
+    : m_Map(10, 10)
+    , m_Players()
+    , m_Eggs()
+    , m_Teams()
 {
 
+}
+
+void Zappy::Game::Game::setTeams(std::span<std::string> names, std::size_t maxMembers)
+{
+    m_Teams.clear();
+
+    for (auto& name : names) {
+        Team team(name, maxMembers);
+
+        auto [randX, randY] = randomPosition();
+        m_Eggs.emplace_back(-1, name, randX, randY);
+
+        m_Teams[std::move(name)] = std::move(team);
+    }
+}
+
+void Zappy::Game::Game::playerLayEgg(const Player& player)
+{
+    auto [x, y] = player.position();
+    auto& egg = m_Eggs.emplace_back(player.id(), player.team(), x, y);
+    m_GraphicsEvents.push_back(Event::eggNew(egg.id(), egg.parentId(), egg.getPosition()));
 }
--- server/src/game/Game/Game.hpp
+++ server/src/game/Game/Game.hpp
@@ -8,10 +8,49 @@
 
 #pragma once
 
+#include "../Resources/Resources.hpp"
+#include "../Player/Player.hpp"
+#include "../Team/Team.hpp"
+#include "../Egg/Egg.hpp"
+#include <zappy/utils.hpp>
+#include <unordered_map>
+#include <random>
+#include <span>
+
 
 namespace Zappy::Game {
     class Game {
+        static std::mt19937 s_RNG;
+
+        public:
+            Utils::Array2D<Resources> m_Map;
+            std::vector<Player> m_Players;
+            std::vector<Egg> m_Eggs;
+            std::unordered_map<std::string, Team> m_Teams;
+
+            std::vector<std::string> m_GraphicsEvents;
+
         public:
             Game() noexcept;
+
+            inline void setMapSize(std::pair<unsigned int, unsigned int> size) { m_Map.resize(size.first, size.second); }
+            void setTeams(std::span<std::string> names, std::size_t maxMembers);
+
+            void playerLayEgg(int playerId) { return playerLayEgg(m_Players[playerId]); }
+            void playerLayEgg(const Player& player);
+
+        private:
+            template <std::integral T>
+            static T random(T start, std::type_identity_t<T> end)
+            {
+                std::uniform_int_distribution<T> dist(start, end);
+                return dist(s_RNG);
+            }
+
+            std::pair<unsigned int, unsigned int> randomPosition() const
+            {
+                auto [x, y] = m_Map.size();
+                return { random(0u, x), random(0u, y) };
+            }
     };
 }
