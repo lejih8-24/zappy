a2cd5fc05be4eb183e940adaf0158a1a002ec1c9
Bard-Gaming <christophe@dronne.fr>
2026-06-25 22:26:47 +0200

    feat(server): implement most of GuiState

--- server/src/client/GuiState/GuiState.cpp
+++ server/src/client/GuiState/GuiState.cpp
@@ -7,15 +7,42 @@
 */
 
 #include "GuiState.hpp"
+#include <zappy/client.hpp>
 
 
 Zappy::Client::GuiState::GuiState()
     : BaseState()
+    , m_QueuedEvents()
 {
 
 }
 
-void Zappy::Client::GuiState::update(Client&, std::chrono::nanoseconds)
+void Zappy::Client::GuiState::init()
 {
+    m_QueuedEvents.emplace_back(Game::Event::mapSize(game().mapSize()));
 
+    for (game().tiles())
+}
+
+void Zappy::Client::GuiState::update(Client& client, std::chrono::nanoseconds dt)
+{
+    // only one event can be sent per update,
+    // so add all new server events to the queue
+    for (const auto& event : game().graphicsEvents()) {
+        m_QueuedEvents.emplace_back(event);
+    }
+
+    if (!m_QueuedEvents.empty()) {
+        client.send(m_QueuedEvents.front());
+        m_QueuedEvents.pop_front();
+    }
+
+    std::string command;
+    if (client.readline(command))
+        handleCommand(command);
+}
+
+void Zappy::Client::GuiState::handleCommand(std::string_view command)
+{
+    // TODO: implement
 }
--- server/src/client/GuiState/GuiState.hpp
+++ server/src/client/GuiState/GuiState.hpp
@@ -9,13 +9,20 @@
 #pragma once
 
 #include "../BaseState.hpp"
+#include <deque>
 
 
 namespace Zappy::Client {
     class GuiState : public BaseState {
+        std::deque<std::string> m_QueuedEvents;
+
         public:
             GuiState();
 
+            void init() override;
             void update(Client& client, std::chrono::nanoseconds dt) override;
+
+        private:
+            void handleCommand(std::string_view command);
     };
 }
