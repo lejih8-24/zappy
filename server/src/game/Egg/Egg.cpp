184f62a81d529db0ab67f09f2f541eca88dea567
Bard-Gaming <christophe@dronne.fr>
2026-06-26 01:08:11 +0200

    feat(server): add team to Egg class

--- server/src/game/Egg/Egg.cpp
+++ server/src/game/Egg/Egg.cpp
@@ -12,9 +12,10 @@
 int Zappy::Game::Egg::s_NextId = 0;
 
 
-Zappy::Game::Egg::Egg(int playerId, unsigned int tileX, unsigned int tileY) noexcept
+Zappy::Game::Egg::Egg(int playerId, std::string_view team, unsigned int tileX, unsigned int tileY) noexcept
     : m_Id(s_NextId)
     , m_PlayerId(playerId)
+    , m_TeamName(team)
     , m_Position{ tileX, tileY }
 {
     s_NextId++;
