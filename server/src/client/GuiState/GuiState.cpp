5550df449b1e71345ff0303e2ec204f86f9ddbb5
Bard-Gaming <christophe@dronne.fr>
2026-06-27 23:12:12 +0200

    feat(server): implement gui msz command

--- server/src/client/GuiState/GuiState.cpp
+++ server/src/client/GuiState/GuiState.cpp
@@ -8,6 +8,20 @@
 
 #include "GuiState.hpp"
 #include <zappy/client.hpp>
+#include <zappy/game.hpp>
+
+
+const std::unordered_map<std::string_view, Zappy::Client::GuiState::CommandHandler> Zappy::Client::GuiState::COMMAND_HANDLERS = {
+    { "msz", mapSizeCommand },
+    { "bct", tileContentsCommand },
+    { "mct", mapContentsCommand },
+    { "tna", teamNamesCommand },
+    { "ppo", playerPositionCommand },
+    { "plv", playerLevelCommand },
+    { "pin", playerInventoryCommand },
+    { "sgt", serverGetTimeCommand },
+    { "sst", serverSetTimeCommand },
+};
 
 
 Zappy::Client::GuiState::GuiState()
@@ -48,10 +62,67 @@ void Zappy::Client::GuiState::postUpdate(Client& client)
 
     std::string command;
     if (client.readline(command))
-        handleCommand(command);
+        handleCommand(command, client);
 }
 
-void Zappy::Client::GuiState::handleCommand(std::string_view command)
+void Zappy::Client::GuiState::handleCommand(std::string_view command, Client& client)
+{
+    auto commandEnd = command.find(' ');
+    std::string_view args = commandEnd + 1 >= command.size() ? std::string_view() : command.substr(commandEnd + 1);
+    command = command.substr(0, commandEnd);
+
+    try {
+        auto handler = COMMAND_HANDLERS.at(command);
+        handler(args, *this, client);
+    } catch (std::out_of_range) {
+        logger.warning() << std::string(client) << " (GUI): unknown command " << std::quoted(command) << std::endl;
+        queueMessage("suc\n");
+        return;
+    }
+}
+
+void Zappy::Client::GuiState::mapSizeCommand(std::string_view, GuiState& state, Client&)
+{
+    state.game().mapSize();
+    state.queueMessage(Game::Event::mapSize(state.game().mapSize()));
+}
+
+void Zappy::Client::GuiState::tileContentsCommand(std::string_view args, GuiState& state, Client& client)
+{
+    // TODO: implement
+}
+
+void Zappy::Client::GuiState::mapContentsCommand(std::string_view args, GuiState& state, Client& client)
+{
+    // TODO: implement
+}
+
+void Zappy::Client::GuiState::teamNamesCommand(std::string_view args, GuiState& state, Client& client)
+{
+    // TODO: implement
+}
+
+void Zappy::Client::GuiState::playerPositionCommand(std::string_view args, GuiState& state, Client& client)
+{
+    // TODO: implement
+}
+
+void Zappy::Client::GuiState::playerLevelCommand(std::string_view args, GuiState& state, Client& client)
+{
+    // TODO: implement
+}
+
+void Zappy::Client::GuiState::playerInventoryCommand(std::string_view args, GuiState& state, Client& client)
+{
+    // TODO: implement
+}
+
+void Zappy::Client::GuiState::serverGetTimeCommand(std::string_view args, GuiState& state, Client& client)
+{
+    // TODO: implement
+}
+
+void Zappy::Client::GuiState::serverSetTimeCommand(std::string_view args, GuiState& state, Client& client)
 {
     // TODO: implement
 }
--- server/src/client/GuiState/GuiState.hpp
+++ server/src/client/GuiState/GuiState.hpp
@@ -13,6 +13,9 @@
 
 namespace Zappy::Client {
     class GuiState : public QueueState<128> {
+        using CommandHandler = void(*)(std::string_view, GuiState&, Client&);
+        static const std::unordered_map<std::string_view, CommandHandler> COMMAND_HANDLERS;
+
         public:
             GuiState();
 
@@ -21,6 +24,16 @@ namespace Zappy::Client {
             void postUpdate(Client& client) override;
 
         private:
-            void handleCommand(std::string_view command);
+            void handleCommand(std::string_view command, Client& client);
+
+            static void mapSizeCommand(std::string_view args, GuiState& state, Client& client);
+            static void tileContentsCommand(std::string_view args, GuiState& state, Client& client);
+            static void mapContentsCommand(std::string_view args, GuiState& state, Client& client);
+            static void teamNamesCommand(std::string_view args, GuiState& state, Client& client);
+            static void playerPositionCommand(std::string_view args, GuiState& state, Client& client);
+            static void playerLevelCommand(std::string_view args, GuiState& state, Client& client);
+            static void playerInventoryCommand(std::string_view args, GuiState& state, Client& client);
+            static void serverGetTimeCommand(std::string_view args, GuiState& state, Client& client);
+            static void serverSetTimeCommand(std::string_view args, GuiState& state, Client& client);
     };
 }
