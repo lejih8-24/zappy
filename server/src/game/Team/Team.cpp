2283d65ca352e4bca7e03306e36f24dac23e5193
Bard-Gaming <christophe@dronne.fr>
2026-06-28 22:07:17 +0200

    feat(server): add maxLevelPlayers to Team struct

--- server/src/game/Team/Team.cpp
+++ server/src/game/Team/Team.cpp
@@ -13,6 +13,7 @@ Zappy::Game::Team::Team() noexcept
     : m_Name()
     , members(0)
     , maxMembers(0)
+    , maxLevelMembers(0)
 {
 
 }
@@ -21,6 +22,7 @@ Zappy::Game::Team::Team(std::string_view name, std::size_t maxMembers)
     : m_Name(name)
     , members(0)
     , maxMembers(maxMembers)
+    , maxLevelMembers(0)
 {
 
 }
